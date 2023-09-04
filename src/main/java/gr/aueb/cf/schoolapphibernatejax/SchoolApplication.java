4b4905961e45f28da059e05f089c85bdb94b8f67
fbozas <fbozas@hotmail.com>
2023-09-04 12:15:10 +0300

    Added project files

---
+++ src/main/java/gr/aueb/cf/schoolapphibernatejax/SchoolApplication.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.schoolapphibernatejax;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class SchoolApplication extends Application {
+
+}
